921e756424e36e8fa47197291bafb59ebab0130f
ambroise <ambroise.berthe@ikomia.com>
2022-02-08 14:14:42 +0100

    Initial commit

---
+++ train_timm_image_classification_process.py
@@ -0,0 +1,212 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import torch.cuda
+from ikomia import core, dataprocess
+from ikomia.dnn import datasetio, dnntrain
+from ikomia.core.task import TaskParam
+import copy
+# Your imports below
+from train_timm_image_classification.utils import train, parser
+import os
+import yaml
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class TrainTimmImageClassificationParam(TaskParam):
+
+    def __init__(self):
+        TaskParam.__init__(self)
+        # Place default value initialization here
+        # Example : self.windowSize = 25
+        self.cfg["model_name"] = "resnet18"
+        self.cfg["custom_cfg"] = False
+        self.cfg["cfg_path"] = ""
+        self.cfg["epochs"] = 10
+        self.cfg["batch_size"] = 4
+        self.cfg["input_size"] = [224, 224]
+        self.cfg["pretrained"] = True
+        self.cfg["output_folder"] = ""
+        self.cfg["learning_rate"] = 0.05
+
+    def setParamMap(self, param_map):
+        # Set parameters values from Ikomia application
+        # Parameters values are stored as string and accessible like a python dict
+        # Example : self.windowSize = int(param_map["windowSize"])
+        self.cfg["model_name"] = param_map["model_name"]
+        self.cfg["custom_cfg"] = eval(param_map["custom_cfg"])
+        self.cfg["cfg_path"] = param_map["cfg_path"]
+        self.cfg["epochs"] = int(param_map["epochs"])
+        self.cfg["batch_size"] = int(param_map["batch_size"])
+        self.cfg["input_size"] = eval(param_map["input_size"])
+        self.cfg["pretrained"] = eval(param_map["pretrained"])
+        self.cfg["output_folder"] = param_map["output_folder"]
+        self.cfg["learning_rate"] = float(param_map["learning_rate"])
+
+    def getParamMap(self):
+        # Send parameters values to Ikomia application
+        # Create the specific dict structure (string container)
+        param_map = core.ParamMap()
+        # Example : paramMap["windowSize"] = str(self.windowSize)
+        param_map["model_name"] = self.cfg["model_name"]
+        param_map["custom_cfg"] = str(self.cfg["custom_cfg"])
+        param_map["cfg_path"] = self.cfg["cfg_path"]
+        param_map["epochs"] = str(self.cfg["epochs"])
+        param_map["batch_size"] = str(self.cfg["batch_size"])
+        param_map["input_size"] = str(self.cfg["input_size"])
+        param_map["pretrained"] = str(self.cfg["pretrained"])
+        param_map["output_folder"] = self.cfg["output_folder"]
+        param_map["learning_rate"] = self.cfg["learning_rate"]
+        return param_map
+
+
+# --------------------
+# - Class which implements the process
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class TrainTimmImageClassification(dnntrain.TrainProcess):
+
+    def __init__(self, name, param):
+        dnntrain.TrainProcess.__init__(self, name, param)
+        # remove default input
+        self.removeInput(0)
+        self.addInput(dataprocess.CPathIO(core.IODataType.FOLDER_PATH))
+        self.stop_train = False
+        self.tb_writer = None
+        # Create parameters class
+        if param is None:
+            self.setParam(TrainTimmImageClassificationParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        return 1
+
+    def run(self):
+        # Core function of your process
+        # Call beginTaskRun for initialization
+        self.beginTaskRun()
+
+        # Get parameters :
+        param = self.getParam()
+
+        self.stop_train = False
+        data_dir = self.getInput(0).getPath()
+
+        if not param.cfg["custom_cfg"]:
+            args = parser.parse_args([data_dir])
+            args.output = os.path.dirname(__file__) + "/output" if param.cfg["output_folder"] == "" else param.cfg[
+                "output_folder"]
+            if not os.path.isdir(args.output):
+                os.mkdirs(args.output)
+
+            num_classes = 0
+            for base, dirs, files in os.walk(param.cfg["dataset_folder"] + "/train"):
+                for directory in dirs:
+                    num_classes += 1
+            args.model = param.cfg["model_name"]
+            args.pretrained = param.cfg["pretrained"]
+            args.num_classes = num_classes
+            args.batch_size = param.cfg["batch_size"]
+            args.epochs = param.cfg["epochs"]
+            args.prefetcher = not args.no_prefetcher
+            args.input_size = [3] + param.cfg["input_size"]
+            args.lr = param.cfg["learning_rate"]
+            args.checkpoint_hist = 5
+            args.cooldown_epochs = 10 if args.epochs > 10 else 0
+            args.epochs = args.epochs - args.cooldown_epochs
+            args.no_aug = True
+
+        elif os.path.isfile(param.cfg["cfg_path"]):
+            with open(param.cfg["cfg_path"], 'r') as f:
+                cfg = yaml.safe_load(f)
+                parser.set_defaults(**cfg)
+                args = parser.parse_args([data_dir])
+        else:
+            print("Config file " + param.cfg["cfg_file"] + " does not exist")
+
+        # Distributed training not yet available in Ikomia
+        args.distributed = False
+        args.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        args.world_size = 1
+        args.rank = 0  # global rank
+        str_datetime = datetime.now().strftime("%d-%m-%YT%Hh%Mm%Ss")
+        tb_logdir = self.getTensorboardLogDir() + "/" + param.cfg["model_name"] + str(param.cfg["input_size"][0]) \
+                    + "/" + str_datetime
+        self.tb_writer = SummaryWriter(tb_logdir)
+        train(args, self.get_stop, self.tb_writer, self.log_metrics)
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+    def get_stop(self):
+        return self.stop_train
+
+    def stop(self):
+        super().stop()
+        self.stop_train = True
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class TrainTimmImageClassificationFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set process information as string here
+        self.info.name = "train_timm_image_classification"
+        self.info.shortDescription = "Train Timm image classification models"
+        self.info.description = "Dataset format :" \
+                                "dataset_folder" \
+                                "--train" \
+                                "----class_1" \
+                                "----class_2" \
+                                "--val" \
+                                "----class_1" \
+                                "----class_2"
+        # relative path -> as displayed in Ikomia application process tree
+        self.info.path = "Plugins/Python"
+        self.info.iconPath = "icons/timm.png"
+        self.info.version = "1.0.0"
+        # self.info.iconPath = "your path to a specific icon"
+        self.info.authors = "Ross Wightman"
+        self.info.article = "PyTorch Image Models"
+        self.info.journal = "GitHub repository"
+        self.info.year = 2019
+        self.info.license = "Apache-2.0 License "
+        # URL of documentation
+        self.info.documentationLink = "https://rwightman.github.io/pytorch-image-models/"
+        # Code source repository
+        self.info.repository = "https://github.com/rwightman/pytorch-image-models"
+        # Keywords used for search
+        self.info.keywords = "image, classification, imagenet, pretrain, pytorch"
+
+    def create(self, param=None):
+        # Create process object
+        return TrainTimmImageClassification(self.info.name, param)
---
+++ train_timm_image_classification_widget.py
@@ -0,0 +1,135 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+from ikomia.utils import pyqtutils, qtconversion
+from train_timm_image_classification.train_timm_image_classification_process import TrainTimmImageClassificationParam
+
+# PyQt GUI framework
+from PyQt5.QtWidgets import *
+import timm
+
+# --------------------
+# - Class which implements widget associated with the process
+# - Inherits PyCore.CWorkflowTaskWidget from Ikomia API
+# --------------------
+class TrainTimmImageClassificationWidget(core.CWorkflowTaskWidget):
+
+    def __init__(self, param, parent):
+        core.CWorkflowTaskWidget.__init__(self, parent)
+
+        if param is None:
+            self.parameters = TrainTimmImageClassificationParam()
+        else:
+            self.parameters = param
+
+        # Create layout : QGridLayout by default
+        self.gridLayout = QGridLayout()
+
+        # Model name
+        timm_models = timm.list_models()
+        self.combo_model = pyqtutils.append_combo(self.gridLayout, "Model Name")
+        for model in timm_models:
+            self.combo_model.addItem(model)
+
+        self.combo_model.setCurrentText(self.parameters.cfg["model_name"])
+        # Input size
+        self.spin_input_h = pyqtutils.append_spin(self.gridLayout, "Input height", self.parameters.cfg["input_size"][0],
+                                                  min=16)
+        self.spin_input_w = pyqtutils.append_spin(self.gridLayout, "Input width", self.parameters.cfg["input_size"][1],
+                                                  min=16)
+        # Epochs
+        self.spin_epochs = pyqtutils.append_spin(self.gridLayout, "Epochs", self.parameters.cfg["epochs"], min=1)
+        # Batch size
+        self.spin_batch_size = pyqtutils.append_spin(self.gridLayout, "Batch size", self.parameters.cfg["batch_size"])
+        # Pretrain
+        self.check_pretrained = pyqtutils.append_check(self.gridLayout, "Pretrained on Imagenet",
+                                                       self.parameters.cfg["pretrained"])
+        # Output folder
+        self.browse_output_folder = pyqtutils.append_browse_file(self.gridLayout, "Output folder",
+                                                                 self.parameters.cfg["output_folder"])
+        # Base learning rate
+        self.double_spin_lr = pyqtutils.append_double_spin(self.gridLayout, "Learning rate",
+                                                           self.parameters.cfg["learning_rate"], step=1e-4)
+        # Custom config
+        self.check_custom_cfg = pyqtutils.append_check(self.gridLayout, "Enable expert mode",
+                                                       self.parameters.cfg["custom_cfg"])
+
+        self.browse_custom_cfg = pyqtutils.append_browse_file(self.gridLayout, "Custom config path",
+                                                              self.parameters.cfg["cfg_path"])
+        # Disable unused widgets when custom config checkbox is checked
+        self.browse_custom_cfg.setEnabled(self.check_custom_cfg.isChecked())
+        self.double_spin_lr.setEnabled(not self.check_custom_cfg.isChecked())
+        self.browse_output_folder.setEnabled(not self.check_custom_cfg.isChecked())
+        self.check_pretrained.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_batch_size.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_epochs.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_input_w.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_input_h.setEnabled(not self.check_custom_cfg.isChecked())
+        self.combo_model.setEnabled(not self.check_custom_cfg.isChecked())
+        self.check_custom_cfg.stateChanged.connect(self.on_check)
+        # PyQt -> Qt wrapping
+        layout_ptr = qtconversion.PyQtToQt(self.gridLayout)
+
+        # Set widget layout
+        self.setLayout(layout_ptr)
+
+    def on_check(self, int):
+        self.browse_custom_cfg.setEnabled(self.check_custom_cfg.isChecked())
+        self.double_spin_lr.setEnabled(not self.check_custom_cfg.isChecked())
+        self.browse_output_folder.setEnabled(not self.check_custom_cfg.isChecked())
+        self.check_pretrained.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_batch_size.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_epochs.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_input_w.setEnabled(not self.check_custom_cfg.isChecked())
+        self.spin_input_h.setEnabled(not self.check_custom_cfg.isChecked())
+        self.combo_model.setEnabled(not self.check_custom_cfg.isChecked())
+
+    def onApply(self):
+        # Apply button clicked slot
+
+        # Get parameters from widget
+        # Example : self.parameters.windowSize = self.spinWindowSize.value()
+        self.parameters.cfg["model_name"] = self.combo_model.currentText()
+        self.parameters.cfg["custom_cfg"] = self.check_custom_cfg.isChecked()
+        self.parameters.cfg["cfg_path"] = self.browse_custom_cfg.path
+        self.parameters.cfg["epochs"] = self.spin_epochs.value()
+        self.parameters.cfg["batch_size"] = self.spin_batch_size.value()
+        self.parameters.cfg["input_size"] = [self.spin_input_h.value(),self.spin_input_w.value()]
+        self.parameters.cfg["pretrained"] = self.check_pretrained.isChecked()
+        self.parameters.cfg["output_folder"] = self.browse_output_folder.path
+        self.parameters.cfg["learning_rate"] = self.double_spin_lr.value()
+
+        # Send signal to launch the process
+        self.emitApply(self.parameters)
+
+
+# --------------------
+# - Factory class to build process widget object
+# - Inherits PyDataProcess.CWidgetFactory from Ikomia API
+# --------------------
+class TrainTimmImageClassificationWidgetFactory(dataprocess.CWidgetFactory):
+
+    def __init__(self):
+        dataprocess.CWidgetFactory.__init__(self)
+        # Set the name of the process -> it must be the same as the one declared in the process factory class
+        self.name = "train_timm_image_classification"
+
+    def create(self, param):
+        # Create widget object
+        return TrainTimmImageClassificationWidget(param, None)

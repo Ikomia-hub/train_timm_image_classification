05c98f3d9b954fad3f87e74a482b522d158b8e06
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:21 +0200

    API change for getProgressSteps() function

--- train_timm_image_classification_process.py
+++ train_timm_image_classification_process.py
@@ -105,7 +105,7 @@ class TrainTimmImageClassification(dnntrain.TrainProcess):
         else:
             self.setParam(copy.deepcopy(param))
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         return 100

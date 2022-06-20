18afe4fddb4111dbcc510c9062164410da491aa9
ambroise <ambroise.berthe@ikomia.com>
2022-06-20 14:58:32 +0200

    Add test file

---
+++ train_timm_image_classification_test.py
@@ -0,0 +1,21 @@
+import logging
+from ikomia.core import task, ParamMap
+from ikomia.utils.tests import run_for_test
+import os
+import ikomia
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train timm image classification =====")
+    input_path = t.getInput(0)
+    input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
+    for model_name in ["resnet18", "resnext50_32x4d"]:
+        params = task.get_parameters(t)
+        params["model_name"] = model_name
+        params["epochs"] = 1
+        params["batch_size"] = 2
+        task.set_parameters(t, params)
+        yield run_for_test(t)

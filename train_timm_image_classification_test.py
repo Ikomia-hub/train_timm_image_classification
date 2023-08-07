af88233306a971d68d205d9332bf7fe6cd540a81
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 17:33:30 +0200

    Fix test file

--- train_timm_image_classification_test.py
+++ train_timm_image_classification_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
 
 logger = logging.getLogger(__name__)
@@ -8,11 +7,11 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train timm image classification =====")
     input_path = t.get_input(0)
-    input_path.setPath(data_dict["datasets"]["classification"]["dataset_classification"])
+    input_path.set_path(data_dict["datasets"]["classification"]["dataset_classification"])
     for model_name in ["resnet18", "resnext50_32x4d"]:
-        params = task.get_parameters(t)
+        params = t.get_parameters()
         params["model_name"] = model_name
-        params["epochs"] = 1
-        params["batch_size"] = 1
-        task.set_parameters(t, params)
+        params["epochs"] = "1"
+        params["batch_size"] = "1"
+        t.set_parameters(params)
         yield run_for_test(t)
